9261b4da877f01fc4116e8cee011aa5629f82add
Mathangi Karunakaran <mathangi100@gmail.com>
2019-04-16 08:32:50 +0530

    Add files via upload
    16/04/2019

---
+++ DocumentAssign/src/main/java/com/mathangi/doc/DocumentAssignApplication.java
@@ -0,0 +1,13 @@
+package com.mathangi.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentAssignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentAssignApplication.class, args);
+	}
+
+}
---
+++ DocumentAssign/src/test/java/com/mathangi/doc/DocumentAssignApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mathangi.doc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocumentAssignApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
